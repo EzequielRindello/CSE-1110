d3cab9768c6192905e3aafbcb8a51bd3f6dfabc7
EzequielRindello <109447017+EzequielRindello@users.noreply.github.com>
2023-03-17 13:26:50 -0300

    Add files via upload

---
+++ CSE 111/Data Time/Discount.py
@@ -0,0 +1,26 @@
+# Use the datetime library
+from datetime import datetime
+current_date = datetime.now()
+day_of_week = current_date.weekday()
+
+# Variables rates
+disc_rate = 0.10
+tax_rate = 0.06
+
+# Display info
+print("This program will let you know if you have 10 % discount")
+sub_total = float(input("Please enter the subtotal: "))
+
+# If statement to know the days of discount
+if sub_total >= 50 and (day_of_week == 1 or day_of_week == 2):
+    discount = round(sub_total * disc_rate, 2)
+    print(f"Discount amount: {discount:.2f}")
+    sub_total -= discount
+
+# Tax calculations.
+TAX = round(sub_total * tax_rate, 2)
+print(f"Sales tax amount: {TAX:.2f}")
+total = sub_total+TAX
+
+# print the result.
+print(f"Total: {total:.2f}")
---
+++ CSE 111/Data Time/Fitness.py
@@ -0,0 +1,72 @@
+# Import datetime
+from datetime import datetime
+
+
+def main():
+    # Get the user's gender, birthdate, height, and weight.
+    gender = str(input("Please enter your gender (M or F): "))
+    birth_str = str(input("Enter your birthdate (YYYY-MM-DD): "))
+    pounds = float(input("Enter your weight in U.S. pounds: "))
+    inches = float(input("Enter your height in U.S. inches: "))
+
+    # Call the compute_age, kg_from_lb, cm_from_in,
+    # body_mass_index, and basal_metabolic_rate functions as needed.
+    years = compute_age(birth_str)
+    kg = kg_from_lb(pounds)
+    cm = cm_from_in(inches)
+    bmi = body_mass_index(kg, cm)
+    bmr = basal_metabolic_rate(gender, kg, cm, years)
+
+    # Print the results for the user to see.
+    print(f"Age (years): {years}")
+    print(f"Weight (kg): {kg:.2f}")
+    print(f"Height (cm): {cm:.1f}")
+    print(f"Body mass index: {bmi:.1f}")
+    print(f"Basal metabolic rate (kcal/day): {bmr:.0f}")
+    pass
+
+
+def compute_age(birth_str):
+    # Convert a person's birthdate from a string
+    # to a date object.
+    birthdate = datetime.strptime(birth_str, "%Y-%m-%d")
+    today = datetime.now()
+
+    # Compute the difference between today and the
+    # person's birthdate in years.
+    years = today.year - birthdate.year
+
+    # If necessary, subtract one from the difference.
+    if birthdate.month > today.month or \
+        (birthdate.month == today.month and
+            birthdate.day > today.day):
+        years -= 1
+
+    return years
+
+
+def kg_from_lb(x):
+    kg = x * 0.45359237
+    return kg
+
+
+def cm_from_in(j):
+    cm = j * 2.54
+    return cm
+
+
+def body_mass_index(x, s):
+    bmi = x / (s ** 2) * 10000
+    return bmi
+
+
+def basal_metabolic_rate(gender, weight, height, age):
+    if gender.upper() == "F":
+        bmr = 447.593 + 9.247 * weight + 3.098 * height - 4.330 * age
+    else:
+        bmr = 88.362 + 13.397 * weight + 4.799 * height - 5.677 * age
+    return bmr
+
+
+if __name__ == "__main__":
+    main()
