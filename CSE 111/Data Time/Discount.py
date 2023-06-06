8aaf4768e8e70c17c77db15e8f8d403ef190c84b
Ezequiel Rindello <rindelloezequiel@gmail.com>
2023-06-05 21:14:14 -0300

    .

--- CSE 111/Data Time/Discount.py
+++ CSE 111/Data Time/Discount.py
@@ -14,6 +14,7 @@ def console_clear():
 
 
 def main():
+    console_clear()
     current_date = datetime.now()
     day_of_week = current_date.weekday()
 
@@ -21,19 +22,26 @@ def main():
     disc_rate = 0.10
     tax_rate = 0.06
 
-    # Display info
-    print("This program will let you know if you have 10 % discount")
-    sub_total = float(input("Please enter the subtotal: "))
+    # Display info and ask fot the total
+    while True:
+        print("This program will let you know if you have 10 % discount\n")
+        try: 
+            sub_total = float(input("Please enter the subtotal: "))
+            break
+        except:
+            print("Error. Please enter a valid choice.")
+            read_key()
+            console_clear()
 
     # If statement to know the days of discount
     if sub_total >= 50 and (day_of_week == 1 or day_of_week == 2):
         discount = round(sub_total * disc_rate, 2)
-        print(f"Discount amount: {discount:.2f}")
+        print(f"\nDiscount amount: {discount:.2f}")
         sub_total -= discount
 
     # Tax calculations.
     TAX = round(sub_total * tax_rate, 2)
-    print(f"Sales tax amount: {TAX:.2f}")
+    print(f"\nSales tax amount: {TAX:.2f}")
     total = sub_total+TAX
 
     # print the result.
